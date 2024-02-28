08e4901b3a692258b3dfcee38f2186ea8efe9d00
Jon Calvin Wetzel <joncalvinwetzel22@gmail.com>
2024-02-28 11:12:44 -0500

    Orange Background track with lanes marked by hashes

--- jgraphscript.sh
+++ jgraphscript.sh
@@ -1,5 +1,5 @@
 make clean
 make
-bin/tracksplits 8 0 0.5 0 > jgr/trackplot.jgr
+bin/tracksplits 8 255 162 68 > jgr/trackplot.jgr
 jgraph -P jgr/trackplot.jgr| ps2pdf - | convert -density 300 - -quality 100 plots/trackplot.jpg
 open plots/trackplot.jpg
--- src/tracksplits.cpp
+++ src/tracksplits.cpp
@@ -9,12 +9,12 @@ using namespace std;
 void plotTrack(int lanes, vector <float> color) {
     printf("newgraph\n");
     printf("xaxis\n");
-    printf("min 0.2 max 2.8 size 5 \n");
-    printf("no_auto_hash_labels mhash 0 hash 1 shash 1\n");
+    printf("min 0 max 100 size 7 \n");
+    printf("no_auto_hash_labels mhash 0 hash %f\n", 100.0/lanes);
     printf("yaxis\n");
-    printf("min 0 max 10 size 6\n");
+    printf("min 0 max 110 size 11\n");
     printf("nodraw\n");
-    printf("newcurve marktype box marksize 2.6 10 cfill %f %f %f pts 1.5 5\n", color[0], color[1], color[2]);  
+    printf("newcurve marktype box marksize 100 110 cfill %f %f %f pts 50 55\n", color[0], color[1], color[2]);  
 
 }
 
@@ -28,9 +28,9 @@ int main(int argc, char** argv) {
     }
     int lanes = atoi(argv[1]);
     vector <float> color;
-    color.push_back(stof(argv[2]));
-    color.push_back(stof(argv[3]));
-    color.push_back(stof(argv[4]));
+    color.push_back(stof(argv[2])/255.0);
+    color.push_back(stof(argv[3])/255.0);
+    color.push_back(stof(argv[4])/255.0);
 /*    for (int i = 0; i < color.size(); i++) {
         cout << color[i] << endl;
     }
