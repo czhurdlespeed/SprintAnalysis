6e2be2728b68b1b844d310e8e03e5a2484b914ba
Jon Calvin Wetzel <joncalvinwetzel22@gmail.com>
2024-02-28 10:59:48 -0500

    Added Directories and Created Shell Script

--- src/tracksplits.cpp
+++ src/tracksplits.cpp
@@ -6,15 +6,15 @@
 using namespace std;
 
 
-void plotTrack(int lanes, vector <int> color) {
+void plotTrack(int lanes, vector <float> color) {
     printf("newgraph\n");
     printf("xaxis\n");
-    printf("min 0 max 10 size 7 \n");
-    printf("no_auto_hash_labels mhash 0 hash 1\n");
+    printf("min 0.2 max 2.8 size 5 \n");
+    printf("no_auto_hash_labels mhash 0 hash 1 shash 1\n");
     printf("yaxis\n");
-    printf("min 0 max 6 size 4\n");
+    printf("min 0 max 10 size 6\n");
     printf("nodraw\n");
-    printf("newcurve marktype box marksize 2.6 10 cfill %d %d %d pts 0 0 10 0 10 5 0 5\n", color[0], color[1], color[2]);  
+    printf("newcurve marktype box marksize 2.6 10 cfill %f %f %f pts 1.5 5\n", color[0], color[1], color[2]);  
 
 }
 
@@ -27,12 +27,14 @@ int main(int argc, char** argv) {
         return 1;
     }
     int lanes = atoi(argv[1]);
-    vector <int> color;
-    string s = argv[2];
-    istringstream iss(s);
-    while (getline(iss, s, ',')) {
-        color.push_back(atoi(s.c_str()));
+    vector <float> color;
+    color.push_back(stof(argv[2]));
+    color.push_back(stof(argv[3]));
+    color.push_back(stof(argv[4]));
+/*    for (int i = 0; i < color.size(); i++) {
+        cout << color[i] << endl;
     }
+*/  
     plotTrack(lanes, color);
 
 
