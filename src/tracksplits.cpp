0e62c587f2903984bd757ed247870c0dcda0d278
Jon Calvin Wetzel <joncalvinwetzel22@gmail.com>
2024-02-28 20:14:08 -0500

    Created maps of splits keyed on time values is Athlete*

--- src/tracksplits.cpp
+++ src/tracksplits.cpp
@@ -2,8 +2,18 @@
 #include<vector>
 #include<string>
 #include<sstream>
+#include<fstream>
+#include<map>
 
 using namespace std;
+
+class Athlete {
+    public:
+        vector <float> splits;
+        float time;
+        string year;
+        string name;
+};
 
 
 void plotTrack(int lanes, vector <float> color) {
@@ -47,15 +57,105 @@ int main(int argc, char** argv) {
         return 1;
     }
     int lanes = atoi(argv[1]);
+    vector <Athlete*> athletes;
     vector <float> color;
+    ifstream file;
     color.push_back(stof(argv[2])/255.0);
     color.push_back(stof(argv[3])/255.0);
     color.push_back(stof(argv[4])/255.0);
-/*    for (int i = 0; i < color.size(); i++) {
-        cout << color[i] << endl;
+    file.open("Data.csv");
+    map <float, Athlete*> reaction; // map of reaction times to athletes
+    map <float, Athlete*> split10m; // map of 10m splits to athletes
+    map <float, Athlete*> split20m; // map of 20m splits to athletes
+    map <float, Athlete*> split30m; // map of 30m splits to athletes
+    map <float, Athlete*> split40m; // map of 40m splits to athletes
+    map <float, Athlete*> split50m; // map of 50m splits to athletes
+    map <float, Athlete*> split60m; // map of 60m splits to athletes
+    map <float, Athlete*> split70m; // map of 70m splits to athletes
+    map <float, Athlete*> split80m; // map of 80m splits to athletes
+    map <float, Athlete*> split90m; // map of 90m splits to athletes
+    map <float, Athlete*> split100m; // map of 100m splits to athletes
+    string line;
+    getline(file, line); // skip header
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string token;
+        Athlete *a = new Athlete();
+        for (int i=0; i<13; i++) {
+            getline(ss, token, ',');
+            cout << token << endl;
+            if (i==0) {
+                a->name = token;
+            } else if (i==1) {
+                a->year = token;
+            } else { // splits[0] == reaction time
+                a->splits.push_back(stof(token));
+            }
+            token.clear();
+            // cases for each split
+            switch (i) {
+                case 2:
+                    reaction[a->splits[0]] = a;
+                    break;
+                case 3:
+                    split10m[a->splits[1]] = a;
+                    break;
+                case 4:
+                    split20m[a->splits[2]] = a;
+                    break;
+                case 5:
+                    split30m[a->splits[3]] = a;
+                    break;
+                case 6:
+                    split40m[a->splits[4]] = a;
+                    break;
+                case 7:
+                    split50m[a->splits[5]] = a;
+                    break;
+                case 8:
+                    split60m[a->splits[6]] = a;
+                    break;
+                case 9:
+                    split70m[a->splits[7]] = a;
+                    break;
+                case 10:
+                    split80m[a->splits[8]] = a;
+                    break;
+                case 11:
+                    split90m[a->splits[9]] = a;
+                    break;
+                case 12:
+                    split100m[a->splits[10]] = a;
+                    break;
+            }
+        }
+        ss.clear();
+        athletes.push_back(a);
     }
-*/  
-    plotTrack(lanes, color);
+
+
+    file.close();
+    for (int i=0; i < athletes.size(); i++) {
+        printf("%s %s\n", athletes[i]->name.c_str(), athletes[i]->year.c_str());
+        for (int j=0; j < athletes[i]->splits.size(); j++) {
+            printf("%f ", athletes[i]->splits[j]);
+        }
+        printf("\n");
+    }
+    float fastestsplit;
+    cout << "Fastest reaction time: " << reaction.begin()->first << " by " << reaction.begin()->second->name << endl;
+    cout << "Fastest 10m split: " << split10m.begin()->first << " by " << split10m.begin()->second->name << endl;
+    cout << "Fastest 20m split: " << split20m.begin()->first << " by " << split20m.begin()->second->name << endl;
+    cout << "Fastest 30m split: " << split30m.begin()->first << " by " << split30m.begin()->second->name << endl;
+    cout << "Fastest 40m split: " << split40m.begin()->first << " by " << split40m.begin()->second->name << endl;
+    cout << "Fastest 50m split: " << split50m.begin()->first << " by " << split50m.begin()->second->name << endl;
+    cout << "Fastest 60m split: " << split60m.begin()->first << " by " << split60m.begin()->second->name << endl;
+    cout << "Fastest 70m split: " << split70m.begin()->first << " by " << split70m.begin()->second->name << endl;
+    cout << "Fastest 80m split: " << split80m.begin()->first << " by " << split80m.begin()->second->name << endl;
+    cout << "Fastest 90m split: " << split90m.begin()->first << " by " << split90m.begin()->second->name << endl;
+    cout << "Fastest 100m split: " << split100m.begin()->first << " by " << split100m.begin()->second->name << endl;
+
+    // plotTrack(lanes, color);
 
 
 
