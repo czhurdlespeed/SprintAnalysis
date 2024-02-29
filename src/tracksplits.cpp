e903f1398eac0e97c1106f5729c66f16a1b06f09
Jon Calvin Wetzel <joncalvinwetzel22@gmail.com>
2024-02-28 21:14:42 -0500

    Created split maps and normalized split maps; ready to start plotting

--- src/tracksplits.cpp
+++ src/tracksplits.cpp
@@ -50,19 +50,9 @@ void plotTrack(int lanes, vector <float> color) {
     }
 }
 
-
-int main(int argc, char** argv) {
-    if (argc < 3) {
-        printf("Usage: %s <lanes> <color>\n", argv[0]);
-        return 1;
-    }
-    int lanes = atoi(argv[1]);
-    vector <Athlete*> athletes;
-    vector <float> color;
+void readData() {
     ifstream file;
-    color.push_back(stof(argv[2])/255.0);
-    color.push_back(stof(argv[3])/255.0);
-    color.push_back(stof(argv[4])/255.0);
+    vector <Athlete*> athletes;
     file.open("Data.csv");
     map <float, Athlete*> reaction; // map of reaction times to athletes
     map <float, Athlete*> split10m; // map of 10m splits to athletes
@@ -154,7 +144,56 @@ int main(int argc, char** argv) {
     cout << "Fastest 80m split: " << split80m.begin()->first << " by " << split80m.begin()->second->name << endl;
     cout << "Fastest 90m split: " << split90m.begin()->first << " by " << split90m.begin()->second->name << endl;
     cout << "Fastest 100m split: " << split100m.begin()->first << " by " << split100m.begin()->second->name << endl;
+    // normalize the splits to the fastest split 
+    vector < map <float, Athlete*> * > accesstable;
+    accesstable.push_back(&reaction);
+    accesstable.push_back(&split10m);
+    accesstable.push_back(&split20m);
+    accesstable.push_back(&split30m);
+    accesstable.push_back(&split40m);
+    accesstable.push_back(&split50m);
+    accesstable.push_back(&split60m);
+    accesstable.push_back(&split70m);
+    accesstable.push_back(&split80m);
+    accesstable.push_back(&split90m);
+    accesstable.push_back(&split100m);
+    map <float, Athlete*>::iterator mit;
+    map <int, map <float, Athlete*> *> normalizedsplits;
+    map <int, map <float, Athlete*> *>::iterator nmit;
+    for (int i = 0; i < accesstable.size(); i++) { // access table to split maps to normalize
+        map <float, Athlete*> * split = accesstable[i]; // maps to normalize
+        map <float, Athlete*> * normalized = new map <float, Athlete*>(); // normalized map
+        float fastestsplit = split->begin()->first; // fastest split from split map
+        for (mit = split->begin(); mit != split->end(); mit++) { // iterate through split map
+            normalized->insert(pair<float, Athlete*>(fastestsplit/mit->first, mit->second)); // insert individual's normalized split into normalized map
+        }
+        normalizedsplits[i] = normalized; // insert normalized map for given split into normalized splits map
+    }
+/*
+ *  NOTE:
+        normalizedsplits < split location, < normalized split, Athlete *> > 
+        Now just go off of normalized splits to plot athletes on the track
+*/
+    for (nmit = normalizedsplits.begin(); nmit != normalizedsplits.end(); nmit++) {
+        cout << "Normalized " << nmit->first << "m split" << endl;
+        for (mit = nmit->second->begin(); mit != nmit->second->end(); mit++) {
+            cout << mit->first << " " << mit->second->name << endl;
+        }
+    }
+}
 
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        printf("Usage: %s <lanes> <color>\n", argv[0]);
+        return 1;
+    }
+    int lanes = atoi(argv[1]);
+    vector <float> color;
+    color.push_back(stof(argv[2])/255.0);
+    color.push_back(stof(argv[3])/255.0);
+    color.push_back(stof(argv[4])/255.0);
+    readData();
     // plotTrack(lanes, color);
 
 
